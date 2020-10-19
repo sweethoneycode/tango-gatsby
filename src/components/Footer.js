023f9fc74a1f0f10c0acb8768117c083d984b3cc
Jonah Brown <support@sweethoneycode.com>
2020-10-18 22:16:45 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,59 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => <div>Footer</div>
+import tangoFB from "../images/tango-facebook-icon.svg";
+import tangoIG from "../images/tango-instagram-icon.svg";
+import tangoLI from "../images/tango-linkedin-icon.svg";
 
-export default Footer
+import { FooterWrapper } from "./styles/FooterStyles";
+
+const Footer = () => (
+  <FooterWrapper>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="whiteText">Contact</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <h3>Joe</h3>
+          <p className="tango-contact">
+            <a href="mailto:">Joe</a>
+          </p>
+          <p className="tango-contect">555-555-5555</p>
+          <span className="social">
+            <a target="_blank" rel="noopener noreferrer" href="#">
+              <img src={tangoLI} alt="tango-li" />
+            </a>
+          </span>
+        </div>
+        <div className="col-md-4">
+          <h3>Joe</h3>
+          <p className="tango-contact">
+            <a href="mailto:">Joe</a>
+          </p>
+          <p className="tango-contect">555-555-5555</p>
+          <span className="social">
+            <a target="_blank" rel="noopener noreferrer" href="#">
+              <img src={tangoLI} alt="tango-li" />
+            </a>
+          </span>
+        </div>
+        <div className="col-md-4">
+          <h3>Joe</h3>
+          <p className="tango-contact">
+            <a href="mailto:">Joe</a>
+          </p>
+          <p className="tango-contect">555-555-5555</p>
+          <span className="social">
+            <a target="_blank" rel="noopener noreferrer" href="#">
+              <img src={tangoLI} alt="tango-li" />
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </FooterWrapper>
+);
+
+export default Footer;
