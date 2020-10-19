55ed898c30b58c9ec7142da83132d3e528a1c296
Jonah Brown <support@sweethoneycode.com>
2020-10-18 21:47:36 -0700

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
 
-const Navigation = () => {
-  return <div>Navigation</div>;
+import { NavigationWrapper } from "./styles/NavigationStyles";
+
+const Navigation = ({ menu }) => (
+  <NavigationWrapper>
+    <ul>
+      {menu.items.map((item, i) => (
+        <li key={i}>
+          <Link to={item.ul} activeClassName="nav-active">
+            {item.title}
+          </Link>
+          {item.wordpress_children ? (
+            <>
+              <span>&#8964;</span>
+              <ul>
+                {item.wordpress_children.map((child, iChild) => (
+                  <li key={iChild}>
+                    <Link to={child.url} activeClassName="nav-active">
+                      {child.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </>
+          ) : null}
+        </li>
+      ))}
+    </ul>
+  </NavigationWrapper>
+);
+
+Navigation.prototypes = {
+  menu: PropTypes.object,
 };
-
 export default Navigation;
