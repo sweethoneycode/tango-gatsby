a895cc2a6ad8158341ec34aae39e4864f61f22e5
Jonah Brown <support@sweethoneycode.com>
2020-10-20 09:49:06 -0700

    Update Citat.js

--- src/components/index/Citat.js
+++ src/components/index/Citat.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
-const Citate = () => <div>Citate</div>;
+import citatimg from "../../images/tango_citat.svg";
+import { CitatWrapper } from "./styles/CitatStyles";
 
-export default Citate;
+const Citat = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wordpressPage(wordpress_id: { eq: 47 }) {
+        acf {
+          citat_1_text
+          citat_1_author
+        }
+      }
+    }
+  `);
+
+  return (
+    <CitatWrapper>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img src={citatimg} alt="quote" />
+            <h6>{data.wordpressPage.acf.citat_1_text}</h6>
+            {data.wordpressPage.acf.citat_1_author}
+          </div>
+        </div>
+      </div>
+    </CitatWrapper>
+  );
+};
+
+export default Citat;
