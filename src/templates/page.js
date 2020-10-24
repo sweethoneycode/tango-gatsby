14024887720b9a8e477d18ca14dd5118b5ff4e3b
Jonah Brown <support@sweethoneycode.com>
2020-10-23 17:15:14 -0700

    creating page templates

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,4 +1,15 @@
-import React from 'react';
+import React from "react";
+import { graphql } from "gatsby";
+import styled from "styled-components";
+
+import Layout from "../components/layout";
+import PageSidebar from "../components/PageSidebar";
+import BreadCrumb from "../components/BreadCrumb";
+import PageHero from "../components/PageHero";
+
+const PageContent = styled.article`
+  margin: 20px 0 0 0;
+`;
 
 const PageTemplate = () => <div>PageTemplate</div>;
 
