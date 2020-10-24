82bf70994ff556d67612a31f7d3f020493b2ba1b
Jonah Brown <support@sweethoneycode.com>
2020-10-23 22:39:58 -0700

    Pagehero and code fixes

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
-import { checkPropTypes, PropType } from "prop-types";
+import { PropTypes } from "prop-types";
 
 import { BreadCrumbWrapper } from "./styles/BreadCrumbStyles";
 
 const BreadCrumb = ({ parent }) => (
   <div className="container">
     <div className="row">
-      <div className=" col-lg-9 offser-lg-3">
-        <BreadCrumb>
+      <div className="col-lg-9 offset-lg-3">
+        <BreadCrumbWrapper>
           <Link to="/">
-            <span>Home</span>
+            <span>Tango Brand Alliance</span>
           </Link>
           <span className="divider">/</span>
           {parent ? (
@@ -21,13 +21,13 @@ const BreadCrumb = ({ parent }) => (
               <span className="divider">/</span>
             </>
           ) : null}
-        </BreadCrumb>
+        </BreadCrumbWrapper>
       </div>
     </div>
   </div>
 );
 
-BreadCrumb.PropTypes = {
+BreadCrumb.propTypes = {
   parent: PropTypes.object,
 };
 
