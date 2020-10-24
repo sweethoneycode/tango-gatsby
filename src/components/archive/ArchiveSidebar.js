ca043cffd4063b64fab44d5e7bcb53a56dec68c3
Jonah Brown <support@sweethoneycode.com>
2020-10-24 16:04:32 -0700

    Update ArchiveSidebar.js

--- src/components/archive/ArchiveSidebar.js
+++ src/components/archive/ArchiveSidebar.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import tangoMail from "../../images/tango-mail-icon.svg";
+import tangoPage from "../../images/tango-page-icon.svg";
+
+import { SidebarWrapper, sidebarMenu } from "./styles/ArchiveSidebarStyles";
 
 const ArchiveSidebar = () => <div>ArchiveSidebar</div>;
 
