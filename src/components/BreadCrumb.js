4862e639050338b6744b48b02ece97f98f8ca200
Jonah Brown <support@sweethoneycode.com>
2020-10-23 21:23:36 -0700

    breadcrumb and page template

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link } from "gatsby";
+import { checkPropTypes, PropType } from "prop-types";
 
-const BreadCrumb = () => <div>BreadCrumb</div>;
+import { BreadCrumbWrapper } from "./styles/BreadCrumbStyles";
+
+const BreadCrumb = ({ parent }) => (
+  <div className="container">
+    <div className="row">
+      <div className=" col-lg-9 offser-lg-3">
+        <BreadCrumb>
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+          <span className="divider">/</span>
+          {parent ? (
+            <>
+              <Link to={parent.link}>
+                <span dangerouslySetInnerHTML={{ __html: parent.title }} />
+              </Link>
+              <span className="divider">/</span>
+            </>
+          ) : null}
+        </BreadCrumb>
+      </div>
+    </div>
+  </div>
+);
+
+BreadCrumb.PropTypes = {
+  parent: PropTypes.object,
+};
 
 export default BreadCrumb;
