a91e54b60d08735dff3f90f1bde2a250809b841b
Jonah Brown <support@sweethoneycode.com>
2020-10-25 11:11:24 -0700

    post sidebar

--- src/components/post/PostSidebar.js
+++ src/components/post/PostSidebar.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "gatsby";
 
+import AuthorIcon from "../../images/tango-author-icon.svg";
+import CategoryIcon from "../../images/tango-category-icon.svg";
+import DateIcon from "../../images/tango-date-icon.svg";
 
-const PostSidebar = () => (
-    <div>Post Sidebar</div>
+import { SidebarWrapper, SidebarMenu } from "./styles/PostSidebarStyles";
+
+const PostSidebar = ({ date, author, categories }) => (
+  <SidebarWrapper className="col-lg-3">
+    <SidebarMenu>
+      <li className="sidebar-menu-header">
+        <img src={DateIcon} alt="date" />
+        <span>{date}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={AuthorIcon} alt="author" />
+        <span>{author}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={CategoryIcon} alt="category" />
+        <span>Categories</span>
+      </li>
+      {categories.map((cat) =>
+        cat.slug !== "alla-trendspaningar" ? (
+          <li key={cat.id}>
+            <Link to={`/trends/${cat.slug}/`}>
+              <span dangerouslySetInnerHTML={{ __html: cat.name }} />
+            </Link>
+          </li>
+        ) : null
+      )}
+    </SidebarMenu>
+  </SidebarWrapper>
 );
 
 export default PostSidebar;
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,59 +1,60 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import { graphql } from "gatsby";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import Layout from '../components/layout';
-import Breadcrumb from '../components/BreadCrumb';
-import PostSidebar from '../components/post/PostSidebar';
+import Layout from "../components/layout";
+import Breadcrumb from "../components/BreadCrumb";
+import PostSidebar from "../components/post/PostSidebar";
 
 const PostContent = styled.article`
-margin: 20px 0 0 0;
+  margin: 20px 0 0 0;
 `;
 
-const postTemplate = ({ data: {post}}) => (
-    <Layout>
-        <Breadcrumb
-            parent={{
-                link: '/trends/alla-trendspaningar',
-                title: 'trends',
-            }}
+const postTemplate = ({ data: { post } }) => (
+  <Layout>
+    <Breadcrumb
+      parent={{
+        link: "/trends/alla-trendspaningar",
+        title: "trends",
+      }}
+    />
+    <div className="container">
+      <div className="row" style={{ marginBottom: "40px" }}>
+        <PostSidebar
+          date={post.date}
+          author={post.author.name}
+          categories={post.categories}
         />
-        <div className="container">
-            <div className="row" style={{ marginBottom: '40px' }}>
-                <PostSidebar
-                    date={post.date}
-                    author={post.author.name}
-                    categories={post.categories}
-                />
-                <PostContent className="col-lg-9">
-                    <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-                    <div dangerouslySetInnerHTML={{ __html: post.content}} />
-                </PostContent>
-            </div>
-        </div>
-</Layout>
-    );
+        <PostContent className="col-lg-9">
+          <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </PostContent>
+      </div>
+    </div>
+  </Layout>
+);
 
 postTemplate.propTypes = {
-    data: PropTypes.object.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
 export default postTemplate;
 
 export const pageQuery = graphql`
-query($id: String!){
-    post: wordpressPost(id: {eq: $id}){
-        title
-        content
-        author{
-            name
-        }
-        date(formatString: "DD, MMM, YYYY")
-        categories {
-            name
-            slug
-        }
+  query($id: String!) {
+    post: wordpressPost(id: { eq: $id }) {
+      title
+      content
+      author {
+        name
+      }
+      date(formatString: "DD, MMM, YYYY")
+      categories {
+        name
+        slug
+        id
+      }
     }
-}
+  }
 `;
